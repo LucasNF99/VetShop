0f048d065d368453bd2c0fdf2945c35d340f6ada
Lucas Natalicio Ferreira <lucas.ferreira@novahaus.com.br>
2020-02-11 18:02:54 -0300

    First commit

---
+++ site/src/pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="m-container-login">
+    <q-form class="o-login-form">
+      <div class="o-login-fields">
+        <h1 class="a-login_title">Login</h1>
+        <q-input
+        v-model="email"
+        color="secondary"
+        class="text-secondary"
+        label="E-mail" />
+        <q-input
+        v-model="password"
+        :type="isPwd ? 'password' : 'text'">
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+        </template>
+        </q-input>
+        <q-btn
+        class="a-btn"
+        color="secondary"
+        text-color="white"
+        label="Login" no-caps />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      isPwd: true,
+      password: '',
+      email: '',
+    };
+  },
+};
+</script>
---
+++ site/src/router/routes.js
@@ -0,0 +1,27 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/index.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('layouts/noHeaderIndex.vue'),
+    children: [
+      { path: '', component: () => import('pages/login.vue') },
+    ],
+  },
+];
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue'),
+  });
+}
+
+export default routes;
