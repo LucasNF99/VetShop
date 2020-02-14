c98675f74ae4ddb013fe0379d018d33d2c540c65
Lucas Natalicio Ferreira <lucas.ferreira@novahaus.com.br>
2020-02-14 15:33:52 -0300

    Working on index page

--- site/src/pages/Index.vue
+++ site/src/pages/Index.vue
@@ -1,13 +1,19 @@
 <template>
   <q-page class="o-index">
+    <q-table class="m-index-table"
+    style="overflow-y: auto"
+      title="Agenda"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+      :pagination.sync="pagination"
+    />
     <q-card class="m-index-card">
       <q-card-section>
-        <div>Our Changing Planet</div>
-        <div>by John Doe</div>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        {{ lorem }}
+        Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Assumenda aut totam facere architecto numquam
+        sit nemo autem culpa saepe, cumque cupiditate, eius
+        excepturi. Obcaecati eligendi at soluta illum voluptatem accusamus?
       </q-card-section>
     </q-card>
   </q-page>
@@ -16,5 +22,64 @@
 <script>
 export default {
   name: 'PageIndex',
+
+  data() {
+    return {
+      pagination: {
+        rowsPerPage: 7,
+      },
+      tab: 'mails',
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Hora',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+        },
+        {
+          name: 'paciente', align: 'left', label: 'Paciente', field: 'paciente',
+        },
+        {
+          name: 'responsavel', align: 'left', label: 'Responsavel', field: 'paciente',
+        },
+      ],
+      data: [
+        {
+          name: '08:00',
+          hora: '13:00',
+          paciente: 'José',
+        },
+        {
+          name: '09:00',
+          hora: '16:30',
+          paciente: 'Safe',
+        },
+        {
+          name: '10:00',
+          hora: '16:30',
+          paciente: 'Safe',
+        },
+        {
+          name: '11:00',
+          hora: '16:30',
+          paciente: 'Safe',
+        },
+        {
+          name: '12:00',
+          hora: '16:30',
+          paciente: 'Safe',
+        },
+        {
+          name: '13:00',
+          hora: '16:30',
+          paciente: 'Safe',
+        },
+
+
+      ],
+    };
+  },
 };
 </script>
