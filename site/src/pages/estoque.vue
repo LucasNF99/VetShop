d005c62090a13213f8dd0205a190d0e5e7d5479c
Lucas Natalicio Ferreira <lucas.ferreira@novahaus.com.br>
2020-02-28 14:13:49 -0300

    Adjusts

--- site/src/pages/estoque.vue
+++ site/src/pages/estoque.vue
@@ -8,25 +8,28 @@
 
       <q-card-section class="m-btn-group">
         <q-btn class="o-btn_estoque"
+        align="left"
         icon="healing"
         no-caps
         to="/estoque-produto-medicamento"
         label="Medicamento"/>
           <q-btn
           no-caps
+          align="left"
           icon="sports_baseball"
           class="o-btn_estoque -flex2"
           label="Brinquedo"
           />
           <q-btn
             no-caps
+            align="left"
             icon="local_dining"
             class="o-btn_estoque -flex2"
             label="Comida"
           />
           <q-btn
             no-caps
-
+            align="left"
             class="a-btn o-btn_estoque -flex2"
             label="Acessório"
           />
@@ -35,7 +38,17 @@
     <q-card class="m-card-nav">
       <q-card-section class="a-card-title">
         <q-icon name="assignment" size="md"/>
-        <div class="text-h6">Items vendidos</div>
+        <div class="text-h6">Dados</div>
+      </q-card-section>
+      <q-card-section>
+        <p>Produto mais vendido:</p>
+        <q-icon size="lg" name="arrow_upward" style="color: green;"></q-icon>
+        <div style="display: inline;">25% - - </div>
+         <p style="display: inline;">Ração Golden Special Sabor Frango e Carne para Cães Adultos</p>
+      </q-card-section>
+      <q-card-section>
+        <p>Produto menos vendido:</p>
+        <q-icon style="color: red;" size="lg" name="arrow_downward"></q-icon>
       </q-card-section>
     </q-card>
   </div>
