5f79d80d441f401dc9abda75a422b95d8a35882b
Clayton Silva <junin-lopes-2008@hotmail.com>
2020-03-05 09:46:48 -0300

    init back-end

--- site/src/pages/caixa.vue
+++ site/src/pages/caixa.vue
@@ -1,25 +1,9 @@
 <template>
   <div>
     <q-toolbar class="text-primary" style="background: #000;">
-      <q-toolbar-title>
-        Caixa
-      </q-toolbar-title>
+      <q-toolbar-title>Caixa</q-toolbar-title>
       <q-btn label="Limpar" />
     </q-toolbar>
-   <qDrawerOpen :leftDrawerOpen="leftDrawerOpen"></qDrawerOpen>
+    <qDrawerOpen :leftDrawerOpen="leftDrawerOpen"></qDrawerOpen>
   </div>
 </template>
-
-<script>
-import qDrawerOpen from '../components/qDrawerOpen';
-
-export default {
-  components: {
-    qDrawerOpen,
-  },
-  data() {
-  return {
-    leftDrawerOpen: true,
-  };
-};
-</script>
