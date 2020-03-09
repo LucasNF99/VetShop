8a48af3828d1bf70c61d511e5d640f026bb129d9
Clayton Silva <junin-lopes-2008@hotmail.com>
2020-03-09 00:56:32 -0300

    add put

--- api/src/app/controllers/ProdutoController.js
+++ api/src/app/controllers/ProdutoController.js
@@ -5,10 +5,10 @@ class ProdutoController {
   async store(req, res) {
     const schema = Yup.object().shape({
       nome: Yup.string().required(),
-      quantidade: Yup.string().required(),
+      quantidade: Yup.number().required(),
       descricao: Yup.string().required(),
-      precoCompra: Yup.string().required(),
-      precoVenda: Yup.string().required(),
+      precoCompra: Yup.number().required(),
+      precoVenda: Yup.number().required(),
       fornecedor: Yup.string().required(),
     })
 
@@ -32,11 +32,12 @@ class ProdutoController {
   async update(req, res) {
     const schema = Yup.object().shape({
       nome: Yup.string(),
-      quantidade: Yup.integer(),
+      quantidade: Yup.number(),
       descricao: Yup.string(),
-      precoCompra: Yup.float(),
-      precoVenda: Yup.float(),
+      precoCompra: Yup.number(),
+      precoVenda: Yup.number(),
       fornecedor: Yup.string(),
+      produtoId: Yup.number().required(),
     })
 
     if (!(await schema.isValid(req.body))) {
@@ -44,12 +45,12 @@ class ProdutoController {
     }
 
 
-    const produto = await Produto.findByPk(req.produtoId);
+    const produto = await Produto.findByPk(req.body.produtoId);
 
-    const { idProduto, nome, quantidade, descricao, precoCompra, precoVenda, fornecedor } = await produto.update(req.body);
+    const { id, nome, quantidade, descricao, precoCompra, precoVenda, fornecedor } = await produto.update(req.body);
 
     return res.json({
-      idProduto,
+      id,
       nome,
       quantidade,
       descricao,
