3147cff164a09b4e90bac52aadfa70b7a1f11ccb
ClaytonJr99 <junin-lopes-2008@hotmail.com>
2020-03-21 17:06:32 -0300

    add multer

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,14 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import ProdutoController from './app/controllers/ProdutoController';
 import UsuarioController from './app/controllers/UsuarioController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/produto', ProdutoController.store);
 routes.post('/usuario', UsuarioController.store);
 
 routes.put('/produto', ProdutoController.update);
 routes.put('/usuario', UsuarioController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+})
 
 export default routes;
