90e9ca136092b15a29df078e6eb373bae4d5ab1e
Lucas Natalicio Ferreira <lucasnf99@gmail.com>
2020-06-24 20:59:40 -0300

    Working on routes.js - front-end

--- site/src/router/routes.js
+++ site/src/router/routes.js
@@ -19,64 +19,14 @@ const routes = [
       { path: 'cliente', component: () => import('pages/cliente.vue') },
       { path: 'gerencia', component: () => import('pages/gerencia.vue') },
       { path: 'estoque-medicamento', component: () => import('pages/medicamento.vue') },
-      { path: 'lista-funcionarios', component: () => import('pages/list-employee.vue') },
-
-    ],
-  },
-  {
-    path: '/gerencia-funcionario',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-employee.vue') },
-    ],
-  },
-  {
-    path: '/gerencia-venda',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-sale.vue') },
-    ],
-  },
-  {
-    path: '/estoque-produto',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/produto.vue') },
-    ],
-  },
-  {
-    path: '/listar-clientes',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-cli.vue') },
-    ],
-  },
-  {
-    path: '/listar-paciente',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-patient.vue') },
-    ],
-  },
-  {
-    path: '/listar-consultas',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-appointment.vue') },
-    ],
-  },
-  {
-    path: '/listar-prontuarios',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/list-record.vue') },
-    ],
-  },
-  {
-    path: '/caixa',
-    component: () => import('layouts/index.vue'),
-    children: [
-      { path: '', component: () => import('pages/caixa.vue') },
+      { path: 'gerencia-funcionario', component: () => import('pages/list-employee.vue') },
+      { path: 'gerencia-venda', component: () => import('pages/list-sale.vue') },
+      { path: 'estoque-produto', component: () => import('pages/produto.vue') },
+      { path: 'listar-clientes', component: () => import('pages/list-cli.vue') },
+      { path: 'listar-paciente', component: () => import('pages/list-patient.vue') },
+      { path: 'listar-consulta', component: () => import('pages/list-appointment.vue') },
+      { path: 'listar-prontuario', component: () => import('pages/list-record.vue') },
+      { path: 'caixa', component: () => import('pages/caixa.vue') },
     ],
   },
   {
